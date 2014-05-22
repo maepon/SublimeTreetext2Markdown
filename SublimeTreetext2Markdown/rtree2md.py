fd5b670b63447d409604327cad7e33a10c338176
maepon <maepon@gmail.com>
2014-05-22 13:57:59 +0900

    start

---
+++ SublimeTreetext2Markdown/rtree2md.py
@@ -0,0 +1,34 @@
+import sublime, sublime_plugin
+import re
+
+class Rtree2mdCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    bufferText = self.view.substr(sublime.Region(0, self.view.size()))
+    mdText = ''
+    lines = bufferText.splitlines()
+
+    reH1 = re.compile(r"^(\d+)\s")
+    reHm = re.compile(r"^((\t+)((\d+\.)+\d))\s")
+
+    for line in lines:
+
+      matchH1 = re.search(reH1,line)
+
+      if (matchH1):
+        if (mdText != ''):
+          mdText += "\n"
+        mdText += line.replace(matchH1.group(1),'#')
+        mdText += "\n"
+        continue
+
+      matchHm = re.search(reHm,line)
+
+      if (matchHm):
+        mdText += "\n"
+        mdText += line.replace(matchHm.group(1),'#' + ('#' * len(matchHm.group(2))))
+        mdText += "\n"
+        continue
+      mdText += re.sub(r"\t+","",line)
+      mdText += "\n"
+
+    self.view.replace(edit, sublime.Region(0, self.view.size()), mdText)
