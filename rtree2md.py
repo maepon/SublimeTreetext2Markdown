ea1fc8bcc3593813f1b87f6f1e61005ed94f3d2c
maepon <maepon@gmail.com>
2014-05-27 15:40:10 +0900

    リスト変換機能追加

--- rtree2md.py
+++ rtree2md.py
@@ -32,3 +32,51 @@ class Rtree2mdCommand(sublime_plugin.TextCommand):
       mdText += "\n"
 
     self.view.replace(edit, sublime.Region(0, self.view.size()), mdText)
+
+class Rtreeheader2listCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    textSelection = [a for a in self.view.sel()][0]
+    resultText = ''
+    startFlag = 0
+    baseHeaderLevel = 0
+    lines = self.view.substr(textSelection).splitlines()
+    baseHeadCheckRe = re.compile(r"^(\#+)\s")
+    listLabelArr = ['-','*']
+
+    for line in lines:
+
+      if (line == '' and startFlag == 0):
+        resultText += "\n"
+        continue
+
+      if (line == ''):
+        continue
+
+      matchHead = re.search(baseHeadCheckRe,line)
+
+      if (matchHead):
+        if (startFlag == 0):
+          baseHeaderLevel = len(matchHead.group(1))
+          startFlag = 1
+
+        listLevel = len(matchHead.group(1)) - baseHeaderLevel
+
+        if (listLevel < 0):
+          resultText += "\n"
+          resultText += line
+          resultText += "\n"
+          continue
+
+        resultText += "\t" * listLevel
+        resultText += listLabelArr[(listLevel + 1) % 2]
+        resultText += " "
+        resultText += re.sub(r"\#+","",line)
+        resultText += "\n"
+        continue
+
+      resultText += line
+      resultText += "\n"
+
+    print(resultText)
+    self.view.replace(edit, textSelection, resultText)
+
